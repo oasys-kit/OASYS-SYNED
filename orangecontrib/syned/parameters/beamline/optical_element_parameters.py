d60023f9a2d8676e961a792c681f9d02c9b66bd6
luca.rebuffi@elettra.eu <Bacilotto_00>
2017-01-26 15:30:48 +0100

    First commit

--- orangecontrib/syned/parameters/beamline/optical_element_parameters.py
+++
@@ -1,6 +0,0 @@
-from syned.beamline.optical_element import OpticalElement
-
-class OpticElementParameters(object):
-    def __init__(self, optical_element=OpticalElement()):
-        self._optical_element=optical_element
-
--- orangecontrib/syned/parameters/beamline/optical_elements/screen/image_plane_parameters.py
+++
@@ -1,8 +0,0 @@
-
-class ImagePlaneParameters(OpticElementParameters):
-    def __init__(self, beamline=Beamline(),
-                 energy_min=1.0,
-                 energy_max=100000.0,
-                 image_plane=ImagePlane("image plane")):
-        super().__init__(beamline=beamline, energy_min=energy_min, energy_max=energy_max)
-        self._image_plane = image_plane
