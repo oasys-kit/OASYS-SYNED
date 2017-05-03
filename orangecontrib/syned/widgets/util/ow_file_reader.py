455129b9fbc108df06e1bd3529c4a630c521c382
Manuel Sanchez del Rio <srio@esrf.eu>
2017-05-03 16:27:34 +0200

    remote file access

--- orangecontrib/syned/widgets/util/ow_file_reader.py
+++ orangecontrib/syned/widgets/util/ow_file_reader.py
@@ -6,7 +6,7 @@ from oasys.widgets import widget as oasyswidget
 
 from syned.storage_ring.light_source import LightSource
 from syned.beamline.beamline import Beamline
-from syned.util.json_tools import load_from_json_file
+from syned.util.json_tools import load_from_json_file, load_from_json_url
 
 class FileReader(oasyswidget.OWWidget):
     name = "Syned File Reader"
@@ -38,14 +38,14 @@ class FileReader(oasyswidget.OWWidget):
         self.setFixedWidth(590)
         self.setFixedHeight(150)
 
-        left_box_1 = oasysgui.widgetBox(self.controlArea, "Syned File Selection", addSpace=True, orientation="vertical",
-                                         width=570, height=60)
+        left_box_1 = oasysgui.widgetBox(self.controlArea, "Syned Local/Remote File Selection", addSpace=True,
+                                        orientation="vertical",width=570, height=60)
 
         figure_box = oasysgui.widgetBox(left_box_1, "", addSpace=True, orientation="horizontal", width=550, height=50)
 
-        self.le_syned_file_name = oasysgui.lineEdit(figure_box, self, "syned_file_name", "Syned File Name",
-                                                    labelWidth=120, valueType=str, orientation="horizontal")
-        self.le_syned_file_name.setFixedWidth(330)
+        self.le_syned_file_name = oasysgui.lineEdit(figure_box, self, "syned_file_name", "Syned File Name or File URL",
+                                                    labelWidth=190, valueType=str, orientation="horizontal")
+        self.le_syned_file_name.setFixedWidth(260)
 
         gui.button(figure_box, self, "...", callback=self.selectFile)
 
@@ -60,12 +60,17 @@ class FileReader(oasyswidget.OWWidget):
         self.le_syned_file_name.setText(oasysgui.selectFileFromDialog(self, self.syned_file_name, "Open Syned File"))
 
     def read_file(self):
+        self.setStatusMessage("")
+        if self.syned_file_name[:4] == "http":
+            is_remote = True
+        else:
+            is_remote = False
 
         try:
-            self.setStatusMessage("")
-
-
-            tmp = load_from_json_file(self.syned_file_name)
+            if is_remote:
+                tmp = load_from_json_url(self.syned_file_name)
+            else:
+                tmp = load_from_json_file(self.syned_file_name)
             print("Reading file %s, returned class: "%self.syned_file_name,type(tmp))
 
             if  isinstance(tmp,LightSource):
@@ -85,7 +90,7 @@ if __name__ == "__main__":
 
     a = QApplication(sys.argv)
     ow = FileReader()
-    ow.syned_file_name = "/scisoft/xop2.4/extensions/shadowvui/shadow3-scripts/ESRF-LIGHTSOURCES/ESRF_ID30_HighBeta_PPU35_23.json"
+    ow.syned_file_name = "" # /scisoft/xop2.4/extensions/shadowvui/shadow3-scripts/ESRF-LIGHTSOURCES/ESRF_ID30_HighBeta_PPU35_23.json"
 
     ow.show()
     a.exec_()
