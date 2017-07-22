8e2bb80724ab07a577fd5c7d54019306c7bd1e5d
Luca Rebuffi <luca.rebuffi@elettra.eu>
2017-07-22 17:57:32 +0200

    SYNED/WOFRY development (SRW/WISE/SHADOWOUI)

--- orangecontrib/syned/widgets/beamline_elements/ow_mirror.py
+++ orangecontrib/syned/widgets/beamline_elements/ow_mirror.py
@@ -1,4 +1,4 @@
-import os, sys
+import numpy
 
 from orangewidget import gui
 from orangewidget.settings import Setting
@@ -25,11 +25,15 @@ class OWMirror(OWOpticalElement):
     width = Setting(0.0)
     height = Setting(0.0)
     radius = Setting(0.0)
+
     min_ax = Setting(0.0)
     maj_ax = Setting(0.0)
 
+    calculate_ellipsoid_parameter = Setting(0)
     min_ax_surface = Setting(0.0)
     maj_ax_surface = Setting(0.0)
+    p_surface = Setting(0.0)
+    q_surface = Setting(0.0)
 
     coating_material = Setting("")
     coating_thickness = Setting(0.0)
@@ -68,7 +72,7 @@ class OWMirror(OWOpticalElement):
 
         self.tab_mir = oasysgui.createTabPage(self.tabs_setting, "Mirror Setting")
 
-        self.surface_shape_box = oasysgui.widgetBox(self.tab_mir, "Surface Shape", addSpace=True, orientation="vertical")
+        self.surface_shape_box = oasysgui.widgetBox(self.tab_mir, "Surface Shape", addSpace=True, orientation="vertical", height=250)
 
         gui.comboBox(self.surface_shape_box, self, "surface_shape", label="Mirror Surface Shape", labelWidth=350,
                      items=["Plane", "Ellipsoid"],
@@ -76,12 +80,23 @@ class OWMirror(OWOpticalElement):
                      sendSelectedValue=False, orientation="horizontal")
 
 
-        self.plane_box = oasysgui.widgetBox(self.surface_shape_box, "", addSpace=False, orientation="vertical", height=60)
+        self.plane_box = oasysgui.widgetBox(self.surface_shape_box, "", addSpace=False, orientation="vertical", height=90)
 
-        self.ellipsoid_box = oasysgui.widgetBox(self.surface_shape_box, "", addSpace=False, orientation="vertical", height=60)
+        self.ellipsoid_box = oasysgui.widgetBox(self.surface_shape_box, "", addSpace=False, orientation="vertical", height=90)
 
-        oasysgui.lineEdit(self.ellipsoid_box, self, "min_ax_surface", "Minor Axis [m]", labelWidth=260, valueType=float, orientation="horizontal")
-        oasysgui.lineEdit(self.ellipsoid_box, self, "maj_ax_surface", "Major Axis [m]", labelWidth=260, valueType=float, orientation="horizontal")
+        gui.comboBox(self.ellipsoid_box, self, "calculate_ellipsoid_parameter", label="Ellispoid Shape", labelWidth=350,
+                     items=["Manual", "Automatic"],
+                     callback=self.set_EllipsoidShape,
+                     sendSelectedValue=False, orientation="horizontal")
+
+        self.ellipsoid_box_1 = oasysgui.widgetBox(self.ellipsoid_box, "", addSpace=False, orientation="vertical", height=60)
+        self.ellipsoid_box_2 = oasysgui.widgetBox(self.ellipsoid_box, "", addSpace=False, orientation="vertical", height=60)
+
+        oasysgui.lineEdit(self.ellipsoid_box_1, self, "min_ax_surface", "Minor Axis [m]", labelWidth=260, valueType=float, orientation="horizontal")
+        oasysgui.lineEdit(self.ellipsoid_box_1, self, "maj_ax_surface", "Major Axis [m]", labelWidth=260, valueType=float, orientation="horizontal")
+
+        oasysgui.lineEdit(self.ellipsoid_box_2, self, "p_surface", "First Focus to Mirror Center (P) [m]", labelWidth=260, valueType=float, orientation="horizontal")
+        oasysgui.lineEdit(self.ellipsoid_box_2, self, "q_surface", "Mirror Center to Second Focus (Q) [m]", labelWidth=260, valueType=float, orientation="horizontal")
 
         self.set_SurfaceShape()
 
@@ -90,6 +105,10 @@ class OWMirror(OWOpticalElement):
         oasysgui.lineEdit(self.coating_box, self, "coating_material", "Material [Chemical Formula]", labelWidth=180, valueType=str, orientation="horizontal")
         oasysgui.lineEdit(self.coating_box, self, "coating_thickness", "Thickness [m]", labelWidth=260, valueType=float, orientation="horizontal")
 
+    def set_EllipsoidShape(self):
+        self.ellipsoid_box_1.setVisible(self.calculate_ellipsoid_parameter==0)
+        self.ellipsoid_box_2.setVisible(self.calculate_ellipsoid_parameter==1)
+
     def set_Shape(self):
         self.rectangle_box.setVisible(self.shape == 0)
         self.circle_box.setVisible(self.shape == 1)
@@ -97,7 +116,13 @@ class OWMirror(OWOpticalElement):
 
     def set_SurfaceShape(self):
         self.plane_box.setVisible(self.surface_shape == 0)
-        self.ellipsoid_box.setVisible(self.surface_shape == 1)
+
+        if self.surface_shape == 1 :
+            self.ellipsoid_box.setVisible(True)
+            self.set_EllipsoidShape()
+        else:
+            self.ellipsoid_box.setVisible(False)
+
 
     def get_optical_element(self):
         if self.shape == 0:
@@ -122,8 +147,15 @@ class OWMirror(OWOpticalElement):
         if self.surface_shape == 0:
             surface_shape = Plane()
         elif self.surface_shape == 1:
-            surface_shape = Ellipsoid(min_axis=self.min_ax_surface,
-                                      maj_axis=self.maj_ax_surface)
+            if self.calculate_ellipsoid_parameter == 0:
+                surface_shape = Ellipsoid(min_axis=self.min_ax_surface,
+                                          maj_axis=self.maj_ax_surface)
+            elif self.calculate_ellipsoid_parameter == 1:
+                surface_shape = Ellipsoid()
+                surface_shape.initialize_from_p_q(self.p_surface, self.q_surface, numpy.radians(90-self.angle_radial))
+
+                self.min_ax_surface = round(surface_shape._min_axis, 4)
+                self.maj_ax_surface = round(surface_shape._maj_axis, 4)
 
         mirror = Mirror(name=self.oe_name,
                         boundary_shape=boundary_shape,
@@ -149,8 +181,12 @@ class OWMirror(OWOpticalElement):
             congruence.checkStrictlyPositiveNumber(self.maj_ax, "(Boundary) Major Axis")
 
         if self.surface_shape == 1:
-            congruence.checkStrictlyPositiveNumber(self.min_ax_surface, "(Surface) Minor Axis")
-            congruence.checkStrictlyPositiveNumber(self.maj_ax_surface, "(Surface) Major Axis")
+            if self.calculate_ellipsoid_parameter == 0:
+                congruence.checkStrictlyPositiveNumber(self.min_ax_surface, "(Surface) Minor Axis")
+                congruence.checkStrictlyPositiveNumber(self.maj_ax_surface, "(Surface) Major Axis")
+            elif self.calculate_ellipsoid_parameter == 1:
+                congruence.checkStrictlyPositiveNumber(self.p_surface, "(Surface) P")
+                congruence.checkStrictlyPositiveNumber(self.q_surface, "(Surface) Q")
 
         congruence.checkEmptyString(self.coating_material, "Coating Material")
         ChemicalFormulaParser.parse_formula(self.coating_material)
