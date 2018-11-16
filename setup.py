9c9fc125adab2f6133455734a0539f902c5ac285
lucarebuffi <lrebuffi@anl.gov>
2018-11-16 14:32:03 -0600

    Height Error Profile management centralized in Oasys + Widgets in Syned to be converted in specific format

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS1-SYNED'
-VERSION = '1.0.2'
+VERSION = '1.0.3'
 ISRELEASED = False
 
 DESCRIPTION = 'SYNED SYNchrotron Elements Dictionary'
@@ -39,7 +39,7 @@ SETUP_REQUIRES = (
 )
 
 INSTALL_REQUIRES = (
-    'oasys1>1.1.15',
+    'oasys1>1.1.16',
     'syned>=1.0.10',
 )
 
