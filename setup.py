9fcf434651add0f061c350d4c77dcda27bd714bf
lucarebuffi <lrebuffi@anl.gov>
2018-11-19 09:53:41 -0600

    Fixed Dependencies

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS1-SYNED'
-VERSION = '1.0.5'
+VERSION = '1.0.6'
 ISRELEASED = False
 
 DESCRIPTION = 'SYNED SYNchrotron Elements Dictionary'
@@ -39,7 +39,7 @@ SETUP_REQUIRES = (
 )
 
 INSTALL_REQUIRES = (
-    'oasys1>=1.1.19',
+    'oasys1>=1.1.21',
 )
 
 PACKAGES = find_packages(exclude=('*.tests', '*.tests.*', 'tests.*', 'tests'))
